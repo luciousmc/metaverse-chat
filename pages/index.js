dc09cd12e9e49c86cb7359d93268cb31e60711e5
Marlon Clay <luciousmc@icloud.com>
2021-12-16 21:44:26 -0800

    adds fonts and styles login page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,26 @@
 import Head from 'next/head';
+import { useMoralis } from 'react-moralis';
 import Login from '../components/Login';
 
 export default function Home() {
-  const isAuthenticated = false;
+  const { isAuthenticated, logout } = useMoralis();
 
-  if (isAuthenticated) return <Login />;
+  if (!isAuthenticated) return <Login />;
 
   return (
     <div className='h-screen'>
       <Head>
         <title>Metavese Challenge</title>
-        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <Login />
+      <h1>User is Logged in</h1>
+
+      <button
+        className='bg-yellow-400 rounded-lg p-4 mt-5 font-bold'
+        onClick={logout}
+      >
+        Logout!
+      </button>
     </div>
   );
 }
